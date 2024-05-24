461c7419fd689625231129cc69cce6909ebc69f5
HGB <hsw8147@gmail.com>
2024-05-25 00:16:31 +0900

    feat: logic update2

--- Config.py
+++ Config.py
@@ -9,6 +9,6 @@ secret = os.getenv("SECRET")
 symbols = ["BTCUSDT", "ETHUSDT", "SOLUSDT"]
 leverages = [5, 5, 3]
 interval = "1h"  # 1h,4h,1d
-ratio = 25  # margin ratio per balance (%)
+ratio = 20  # margin ratio per balance (%)
 profit_ratio = 3  # take profit ratio for whole balance (%)
 stop_ratio = 1.5  # stop loss ratio for whole balance (%)
